2f2603c3a00448865d4c8b1bae88ad80c1940199
Faisal Hussain Alanazi <58009426+1510is10l@users.noreply.github.com>
2022-03-07 20:33:17 +0300

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="description" content="كفراتي هو نقطة بيع بين العميل و الشركة أطلب كفرك و أنت في بيتك" />
     <meta name="keywords" content="كفرات, سيارات, كفراتي, سيارة, تصليح, صيانة, عالم السيارات, بيع, شراء" />
-    <link rel="shortcut icon" href="./assets/images/fav.png" type="image/x-icon">
+    <link rel="shortcut icon" href="./assets/images/fav.png" type="image/x-icon"/>
     <title>عربة التسوق</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
